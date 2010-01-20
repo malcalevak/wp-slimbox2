e6edfbe9ba15ae9e017ccdcf02125a612831662b
malcalevak <malcalevak@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2010-01-20 14:42:31 +0000

    Bumped index.php version to correct, 1.0.1
    git-svn-id: https://plugins.svn.wordpress.org/wp-slimbox2/trunk@195822 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- index.php
+++ index.php
@@ -4,7 +4,7 @@ Plugin Name: WP-Slimbox2
 Plugin URI: http://transientmonkey.com/wp-slimbox2
 Description: A Wordpress implementation of the Slimbox2 javascript, utilizing jQuery, originally written by Christophe Beyls. Requires WP 2.6+
 Author: Greg Yingling (malcalevak)
-Version: 1.0
+Version: 1.0.1
 Author URI: http://transientmonkey.com/
 
 Copyright 2010 Transient Monkey, LLC
